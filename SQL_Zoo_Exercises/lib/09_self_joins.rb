47a6fc9145b4d6b9827c02a6bf5181ef97d2311b
Frank Pak <fpak210@gmailc.om>
2016-12-27 16:49:18 -0800

    complete SQL zoo exercises

--- SQL_Zoo_Exercises/lib/09_self_joins.rb
+++ SQL_Zoo_Exercises/lib/09_self_joins.rb
@@ -103,6 +103,18 @@ def cl_to_lr
   # Craiglockhart, without changing routes. Change the query so that it
   # shows the services from Craiglockhart to London Road.
   execute(<<-SQL)
+  SELECT
+      a.company,
+      a.num,
+      a.stop_id,
+      b.stop_id
+    FROM
+      routes a
+    JOIN
+      routes b ON a.company = b.company AND a.num = b.num
+    WHERE
+      a.stop_id = 53 AND b.stop_id = 149
+
   SQL
 end
 
@@ -130,6 +142,22 @@ def cl_to_lr_by_name
   # number. Change the query so that the services between 'Craiglockhart' and
   # 'London Road' are shown.
   execute(<<-SQL)
+  SELECT
+      a.company,
+      a.num,
+      stopa.name,
+      stopb.name
+    FROM
+      routes a
+    JOIN
+      routes b ON a.company = b.company AND a.num = b.num
+    JOIN
+      stops stopa ON a.stop_id = stopa.id
+    JOIN
+      stops stopb ON b.stop_id = stopb.id
+    WHERE
+      stopa.name = 'Craiglockhart' AND stopb.name = 'London Road'
+
   SQL
 end
 
@@ -137,6 +165,17 @@ def haymarket_and_leith
   # Give the company and num of the services that connect stops
   # 115 and 137 ('Haymarket' and 'Leith')
   execute(<<-SQL)
+  SELECT DISTINCT
+    start_routes.company,
+    start_routes.num
+  FROM
+    routes AS start_routes
+  JOIN
+    routes AS end_routes ON start_routes.company = end_routes.company
+      AND start_routes.num = end_routes.num
+  WHERE
+    start_routes.stop_id = 115 AND end_routes.stop_id = 137
+
   SQL
 end
 
@@ -144,6 +183,30 @@ def craiglockhart_and_tollcross
   # Give the company and num of the services that connect stops
   # 'Craiglockhart' and 'Tollcross'
   execute(<<-SQL)
+  SELECT
+      start_routes.company,
+      start_routes.num
+    FROM
+      routes AS start_routes
+    JOIN
+      routes AS end_routes ON start_routes.company = end_routes.company
+        AND start_routes.num = end_routes.num
+    WHERE
+      start_routes.stop_id = (
+        SELECT
+          id
+        FROM
+          stops
+        WHERE
+          stops.name = 'Craiglockhart'
+      ) AND end_routes.stop_id = (
+        SELECT
+          id
+        FROM
+          stops
+        WHERE
+          stops.name = 'Tollcross'
+      )
   SQL
 end
 
@@ -152,6 +215,26 @@ def start_at_craiglockhart
   # by taking one bus, including 'Craiglockhart' itself. Include the stop name,
   # as well as the company and bus no. of the relevant service.
   execute(<<-SQL)
+  SELECT
+      end_route_stops.name,
+      end_routes.company,
+      end_routes.num
+    FROM
+      routes AS start_routes
+    JOIN
+      routes AS end_routes ON start_routes.num = end_routes.num
+        AND start_routes.company = end_routes.company
+    JOIN
+      stops AS end_route_stops ON end_routes.stop_id = end_route_stops.id
+    WHERE
+      start_routes.stop_id = (
+        SELECT
+          stops.id
+        FROM
+          stops
+        WHERE
+          stops.name = 'Craiglockhart'
+      )
   SQL
 end
 
@@ -160,5 +243,40 @@ def craiglockhart_to_sighthill
   # Sighthill. Show the bus no. and company for the first bus, the name of the
   # stop for the transfer, and the bus no. and company for the second bus.
   execute(<<-SQL)
+  SELECT DISTINCT
+      start.num,
+      start.company,
+      transfer.name,
+      finish.num,
+      finish.company
+    FROM
+      routes start
+    JOIN
+      routes AS to_transfer ON start.company = to_transfer.company
+        AND start.num = to_transfer.num
+    JOIN
+      stops AS transfer ON to_transfer.stop_id = transfer.id
+    JOIN
+      routes AS from_transfer ON transfer.id = from_transfer.stop_id
+    JOIN
+      routes AS finish ON from_transfer.company = finish.company
+        AND from_transfer.num = finish.num
+    WHERE
+      start.stop_id = (
+        SELECT
+          id
+        FROM
+          stops
+        WHERE
+          name = 'Craiglockhart'
+      ) AND finish.stop_id = (
+        SELECT
+          id
+        FROM
+          stops
+        WHERE
+          name = 'Sighthill'
+      )
+
   SQL
 end
